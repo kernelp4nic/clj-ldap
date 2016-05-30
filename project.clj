7c69cc13ea9fd04f437bae967b0d4104cbb835b9
Sebastián Moreno <smoreno.uy@gmail.com>
2016-05-30 15:48:20 -0300

    publish a forked version with downgraded clojure

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.pntblnk/clj-ldap "0.0.12"
+(defproject org.clojars.intception/clj-ldap "0.0.13"
   :description "Clojure ldap client."
   :url "https://github.com/pauldorman/clj-ldap"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.unboundid/unboundid-ldapsdk "3.1.1"]]
   :profiles {:test {:dependencies [[lein-clojars "0.9.1"]]}}
   :aot [clj-ldap.client]
